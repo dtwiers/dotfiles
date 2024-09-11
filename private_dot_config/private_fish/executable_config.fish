8724a785bfc7923d2d99bfe63de787bd93afcba8
Derek Wiers <dtwiers08@gmail.com>
2024-09-11 16:59:14 -0500

    fzf, mise, wt.toml, git default=upstream

--- private_dot_config/private_fish/executable_config.fish
+++ private_dot_config/private_fish/executable_config.fish
@@ -5,6 +5,10 @@ if status is-interactive
     if test -z "$ASDF_HOME" -o ! -f "$ASDF_HOME"
         echo "Error: \$ASDF_HOME is not set or does not point to a valid file."
     end
+    fzf --fish | source
+    if type -q 'mise'
+        mise activate fish | source
+    end
 end
 source $ASDF_HOME
 
