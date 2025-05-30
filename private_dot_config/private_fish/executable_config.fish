4d19cbcb0c6abd57273e96eda6a057946ff34818
Derek Wiers <dtwiers08@gmail.com>
2025-05-30 12:33:31 -0500

    update from desktop, updating nvim & taking out asdf

--- private_dot_config/private_fish/executable_config.fish
+++ private_dot_config/private_fish/executable_config.fish
@@ -2,9 +2,6 @@ if status is-interactive
     starship init fish | source
     fastfetch || neofetch
     zoxide init fish | source
-    if test -z "$ASDF_HOME" -o ! -f "$ASDF_HOME"
-        echo "Error: \$ASDF_HOME is not set or does not point to a valid file."
-    end
     fzf --fish | source
     if type -q 'mise'
         mise activate fish | source
