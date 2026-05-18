0ed153034ca8f474e2c507d10c6dc3da8a846e7c
Derek Wiers <dtwiers08@gmail.com>
2026-05-18 13:54:57 -0500

    add diffview

---
+++ private_dot_config/nvim/lua/eezo/plugins/diffview.lua
@@ -0,0 +1,20 @@
+return {
+    "sindrets/diffview.nvim",
+    config = function()
+        require("diffview").setup {
+            enhanced_diff_hl = true,
+            use_icons = true,
+            signs = {
+                fold_closed = "",
+                fold_open = "",
+            },
+            file_panel = {
+                listing_style = "tree",
+                tree_options = {
+                    flatten_dirs = true,
+                    folder_statuses = "only_folded",
+                },
+            },
+        }
+    end,
+}
